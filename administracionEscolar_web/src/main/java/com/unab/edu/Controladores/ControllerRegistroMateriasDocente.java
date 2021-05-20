83a2ab4f63e542cf19f0158ad452dbc6eaf3c440
EmanuelOrellana1 <63487564+EmanuelOrellana1@users.noreply.github.com>
2021-05-20 15:26:45 -0600

    ControllerMateriasDocente

--- administracionEscolar_web/src/main/java/com/unab/edu/Controladores/ControllerRegistroMateriasDocente.java
+++ administracionEscolar_web/src/main/java/com/unab/edu/Controladores/ControllerRegistroMateriasDocente.java
@@ -5,6 +5,12 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import com.google.gson.Gson;
+import com.unab.edu.DAO.CLSEstudiante;
+import com.unab.edu.DAO.CLSMateria;
+import com.unab.edu.Entidades.Materia;
 
 /**
  * Servlet implementation class ControllerRegistroMateriasDocente
@@ -33,7 +39,13 @@ public class ControllerRegistroMateriasDocente extends HttpServlet {
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		doGet(request, response);
+		//doGet(request, response);
+		
+		Gson json = new Gson();
+
+		CLSMateria clsMateria = new CLSMateria();
+		response.setCharacterEncoding("UTF8");
+		response.getWriter().append(json.toJson(clsMateria.MostrarMateria()));
 	}
 
 }
